0120f0431eff35bc86e1ec95841ad936437a1eee
HChopra22 <127562870+HChopra22@users.noreply.github.com>
2024-04-05 18:59:21 +0100

    Add files via upload

---
+++ Scripts/Misc/AudioManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+using System;
+
+//A class that takes an array of audios and manages them all in one gameObject
+public class AudioManager : MonoBehaviour
+{
+
+    public Sound[] sounds;
+
+    //Whenever there is a sound in the array use these variables
+    void Awake()
+    {
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    //On load, play the background music
+    private void Start()
+    {
+        Play("Theme");
+    }
+
+    //Play, finds the sound name and plays it in scene
+    public void Play (string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.Play();
+    }
+}
